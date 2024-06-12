a6d1c9761b96fce7a0769379d76b6f25578d7bab
sahil.gupta <sahil.gupta@tigeranalytics.com>
2024-06-12 12:04:12 +0530

    worspace built

--- framework_java/test_jv_web/src/main/java/com/ta/api/framework/models/graphql/Data.java
+++
@@ -1,9 +0,0 @@
-package com.ta.api.framework.models.graphql;
-
-public class Data{
-	private InsertTodos insertTodos;
-
-	public InsertTodos getInsertTodos(){
-		return insertTodos;
-	}
-}
--- framework_java/test_jv_web/src/main/java/com/ta/api/framework/models/graphql/GraphqlResponse.java
+++
@@ -1,9 +0,0 @@
-package com.ta.api.framework.models.graphql;
-
-public class GraphqlResponse{
-	private Data data;
-
-	public Data getData(){
-		return data;
-	}
-}
