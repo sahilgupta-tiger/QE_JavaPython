7abee087b0db9a0bacb361551e896a7ad86fbe94
sahil.gupta <sahil.gupta@tigeranalytics.com>
2024-06-13 19:58:26 +0530

    Update GenerateReport.bat

--- framework_java/test_java_web/target/GenerateReport.bat
+++ framework_java/test_java_web/target/GenerateReport.bat
@@ -7,8 +7,8 @@ powershell.exe "Remove-Item allure-results\history\* -Recurse"
 powershell.exe "Remove-Item allure-results\widgets\* -Recurse"
 powershell.exe "Start-Sleep -s 2"
 echo ::Copy the newly generated History and Widgets files to Results folder::
-powershell.exe "Copy-Item -Path allure-report\history\* -Destination allure-results\history -Exclude retry-trend.json,duration-trend.json -PassThru"
-powershell.exe "Copy-Item -Path allure-report\widgets\* -Destination allure-results\widgets -Exclude retry-trend.json,duration-trend.json -PassThru"
+powershell.exe "Copy-Item -Path allure-report\history\* -Destination allure-results\history -PassThru"
+powershell.exe "Copy-Item -Path allure-report\widgets\* -Destination allure-results\widgets -PassThru"
 powershell.exe "Copy-Item -Path environment.properties -Destination allure-results\environment.properties -PassThru"
 powershell.exe "Start-Sleep -s 2"
 echo ::Start the Allure Server to view Reports::
