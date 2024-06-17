9e99d921a3acf2c76bc6846b2f65d4a7883e77d8
sahil.gupta <sahil.gupta@tigeranalytics.com>
2024-06-17 13:13:59 +0530

    Delete framework_java/test_java_web directory

--- framework_java/test_java_web/target/EmptyResults.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-title Empty Out Allure Results
-echo ::Deleting all files from Allure Results folder and its sub-folders::
-powershell.exe "Get-ChildItem -Path 'allure-results' -File -Recurse -Exclude environment.properties -Force | Remove-Item -Force"
