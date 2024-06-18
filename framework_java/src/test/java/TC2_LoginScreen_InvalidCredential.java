c7afe6957deaa57b2fa12db7a264c6cecbeadf30
Prashant Singh <prashant.singh@tigeranalytics.local>
2024-06-18 15:46:04 +0530

    updated Login Screen Invalid Credential File

--- framework_java/src/test/java/TC2_LoginScreen_InvalidCredential.java
+++ framework_java/src/test/java/TC2_LoginScreen_InvalidCredential.java
@@ -1,10 +1,40 @@
 import com.microsoft.playwright.*;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 
 public class TC2_LoginScreen_InvalidCredential {
+
+    Browser browser;
+    Page page;
+    Playwright playwright;
+
+    @BeforeMethod
+    @Parameters({"browser"})
+    public void setup(String browsername){
+
+        BrowserType browserType;
+        playwright=Playwright.create();
+        switch (browsername){
+            case "firefox":
+                browserType=playwright.firefox();
+                break;
+            case "webkit":
+                browserType=playwright.webkit();
+                break;
+            default:
+                throw new IllegalArgumentException("Please provide valid browser name");
+
+        }
+
+        browser=browserType.launch(new BrowserType.LaunchOptions().setHeadless(false).setSlowMo(1000));
+        page=browser.newPage();
+
+    }
+
 
 @Test
 public void Login2() {
@@ -15,10 +45,10 @@ public void Login2() {
 //        BrowserContext browserContext=browser.newContext();
 //        Page page=browserContext.newPage();
 
-    Playwright playwright=Playwright.create();
-    Browser browser=playwright.webkit().launch(new BrowserType.LaunchOptions().setHeadless(false).setSlowMo(1000));
-    BrowserContext context=browser.newContext(new Browser.NewContextOptions().setViewportSize(1000,1080));
-    Page page=context.newPage();
+//    Playwright playwright=Playwright.create();
+//    Browser browser=playwright.webkit().launch(new BrowserType.LaunchOptions().setHeadless(false).setSlowMo(1000));
+//    BrowserContext context=browser.newContext(new Browser.NewContextOptions().setViewportSize(1000,1080));
+//    Page page=context.newPage();
 
     //Go to https://www.saucedemo.com/
     page.navigate("https://www.saucedemo.com/");
