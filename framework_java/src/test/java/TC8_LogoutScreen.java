77796936ae33c527c4c74c9e14547aba7948d81a
Prashant Singh <prashant.singh@tigeranalytics.local>
2024-06-18 15:52:28 +0530

    Updated logout screen

--- framework_java/src/test/java/TC8_LogoutScreen.java
+++ framework_java/src/test/java/TC8_LogoutScreen.java
@@ -1,9 +1,39 @@
 import com.microsoft.playwright.*;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 
 public class TC8_LogoutScreen {
+
+    Browser browser;
+    Page page;
+    Playwright playwright;
+
+    @BeforeMethod
+    @Parameters({"browser"})
+    public void setup(String browsername){
+
+        BrowserType browserType;
+        playwright=Playwright.create();
+        switch (browsername){
+            case "firefox":
+                browserType=playwright.firefox();
+                break;
+            case "webkit":
+                browserType=playwright.webkit();
+                break;
+            default:
+                throw new IllegalArgumentException("Please provide valid browser name");
+
+        }
+
+        browser=browserType.launch(new BrowserType.LaunchOptions().setHeadless(false).setSlowMo(1000));
+        page=browser.newPage();
+
+    }
+
 
 @Test
 public void Logout(){
@@ -14,10 +44,10 @@ public void Logout(){
 //        BrowserContext browserContext=browser.newContext();
 //        Page page=browserContext.newPage();
 
-    Playwright playwright=Playwright.create();
-    Browser browser=playwright.webkit().launch(new BrowserType.LaunchOptions().setHeadless(false).setSlowMo(1000));
-    BrowserContext context=browser.newContext(new Browser.NewContextOptions().setViewportSize(1000,1080));
-    Page page=context.newPage();
+//    Playwright playwright=Playwright.create();
+//    Browser browser=playwright.webkit().launch(new BrowserType.LaunchOptions().setHeadless(false).setSlowMo(1000));
+//    BrowserContext context=browser.newContext(new Browser.NewContextOptions().setViewportSize(1000,1080));
+//    Page page=context.newPage();
 
     //Go to https://www.saucedemo.com/
     page.navigate("https://www.saucedemo.com/");
