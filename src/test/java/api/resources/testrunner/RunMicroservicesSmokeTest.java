c53f3f43b2972a81d0b41dc4ab9d8d2be2a4b01b
yathish.gowda <yathish.gowda@tigeranalytics.com>
2025-09-01 17:23:00 +0530

    Minor Changes

--- src/test/java/api/resources/testrunner/RunMicroservicesSmokeTest.java
+++ src/test/java/api/resources/testrunner/RunMicroservicesSmokeTest.java
@@ -11,7 +11,7 @@ import static allurereportGeneration.allureReportGeneration.*;
 @CucumberOptions(
         features = "src/test/java/api/resources/features",
         glue = {"api/stepdefinitions"},
-        tags = "@smoke",
+        tags = "@hazard",
         plugin = {
                 "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
                 "pretty",
